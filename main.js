0ec135c79dc9e2d24f5f6f557352a8e39b50f65a
Cole Hammond <cole.d.hammond@hotmail.co.nz>
2022-12-05 22:27:17 +1300

    0.0.3

--- main.js
+++ main.js
@@ -4,86 +4,100 @@ const config = require("./config.json");
 
 //#region DJS Setup
 const client = new Client({
-    intents: [ GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.DirectMessages, GatewayIntentBits.MessageContent ], 
-    partials: [ Partials.User, Partials.Channel, Partials.GuildMember, Partials.Message, Partials.Reaction ] 
+	intents: [ GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.DirectMessages, GatewayIntentBits.MessageContent ], 
+	partials: [ Partials.User, Partials.Channel, Partials.GuildMember, Partials.Message, Partials.Reaction ] 
+});
+
+// This event handler is called when the client is ready
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.tag}!`);
 });
 
 // Generate a random DnD character
 function generateCharacter() {
-    // Create an empty character object
-    let character = {};
-  
-    // Generate random abilities
-    character.abilities = {
-      strength: Math.floor(Math.random() * 20) + 1,
-      dexterity: Math.floor(Math.random() * 20) + 1,
-      constitution: Math.floor(Math.random() * 20) + 1,
-      intelligence: Math.floor(Math.random() * 20) + 1,
-      wisdom: Math.floor(Math.random() * 20) + 1,
-      charisma: Math.floor(Math.random() * 20) + 1
-    };
-    
-    // Generate a random name    
-    const names = fs.readFileSync('names.txt', 'utf8').split('\n');
-    character.name = names[Math.floor(Math.random() * names.length)];
-    // Generate a random race
-    const races = ['Human', 'Dwarf', 'Elf', 'Halfling', 'Dragonborn', 'Gnome', 'Half-elf', 'Half-orc', 'Tiefling'];
-    character.race = races[Math.floor(Math.random() * races.length)];
-  
-    // Generate a random class
-    const classes = ['Barbarian', 'Bard', 'Cleric', 'Druid', 'Fighter', 'Monk', 'Paladin', 'Ranger', 'Rogue', 'Sorcerer', 'Warlock', 'Wizard'];
-    character.class = classes[Math.floor(Math.random() * classes.length)];
-  
-    // Generate a random alignment
-    const alignments = ['lawful good', 'neutral good', 'chaotic good', 'lawful neutral', 'neutral', 'chaotic neutral', 'lawful evil', 'neutral evil', 'chaotic evil'];
-    character.alignment = alignments[Math.floor(Math.random() * alignments.length)];
+	// Create an empty character object
+	const character = {};
+	
+	// Generate random abilities
+	character.abilities = {
+	strength: Math.floor(Math.random() * 20) + 1,
+	dexterity: Math.floor(Math.random() * 20) + 1,
+	constitution: Math.floor(Math.random() * 20) + 1,
+	intelligence: Math.floor(Math.random() * 20) + 1,
+	wisdom: Math.floor(Math.random() * 20) + 1,
+	charisma: Math.floor(Math.random() * 20) + 1
+	};
+	
+	// Read the names, races, classes, and alignments from their respective files
+	const names = fs.readFileSync('names.txt', 'utf8').split('\n');
+	const races = fs.readFileSync('races.txt', 'utf8').split('\n');
+	const classes = fs.readFileSync('classes.txt', 'utf8').split('\n');
+	const alignments = fs.readFileSync('alignments.txt', 'utf8').split('\n');
+	
+	// Generate a random name, race, class, and alignment
+	character.name = names[Math.floor(Math.random() * names.length)];
+	character.race = races[Math.floor(Math.random() * races.length)];
+	character.class = classes[Math.floor(Math.random() * classes.length)];
+	character.alignment = alignments[Math.floor(Math.random() * alignments.length)];
 
-    // Generate character's backstory
-    character.backstory = "";
-    const pastEvents = fs.readFileSync('pastEvents.txt', 'utf8').split('\n');
-    let past = pastEvents[Math.floor(Math.random() * pastEvents.length)];
-    character.backstory += `${character.name} ${past}. `;
+	// remove new line from name, race, class, and alignment
+	character.name = character.name.replace(/\r?\n|\r/g, " ");
+	character.race = character.race.replace(/\r?\n|\r/g, " ");
+	character.class = character.class.replace(/\r?\n|\r/g, " ");
+	character.alignment = character.alignment.replace(/\r?\n|\r/g, " ");
+	
+	// Generate character's backstory
+	character.backstory = "";
+	
+	// Read the past events, defining moments, and goals from their respective files
+	const pastEvents = fs.readFileSync('pastEvents.txt', 'utf8').split('\n');
+	const definingMoments = fs.readFileSync('definingMoments.txt', 'utf8').split('\n');
+	const goals = fs.readFileSync('goals.txt', 'utf8').split('\n');
+	
+	// Generate a random past event, defining moment, and goal
+	var past = pastEvents[Math.floor(Math.random() * pastEvents.length)];
+	var definingMoment = definingMoments[Math.floor(Math.random() * definingMoments.length)];
+	var goal = goals[Math.floor(Math.random() * goals.length)];
 
-    const definingMoments = fs.readFileSync('pastEvents.txt', 'utf8').split('\n');
-    let definingMoment = definingMoments[Math.floor(Math.random() * definingMoments.length)];
-    character.backstory +=`One defining moment in their past was when they ${definingMoment}. `;
-
-    const goals = fs.readFileSync('pastEvents.txt', 'utf8').split('\n');
-    let goal = goals[Math.floor(Math.random() * goals.length)];
-    character.backstory += `Currently, ${character.name} is ${goal}.`;
-  
-    // Return the character
-    return character;
+	// remove new line from name, race, class, and alignment
+	past = past.replace(/\r?\n|\r/g, " ");
+	definingMoment = definingMoment.replace(/\r?\n|\r/g, " ");
+	goal= goal.replace(/\r?\n|\r/g, " ");
+	
+	// Add the past event, defining moment, and goal to the character's backstory
+	character.backstory += `${character.name} ${past}. One defining moment in their past was when they ${definingMoment}. Currently, ${character.name} is ${goal}.`;
+	
+	// Return the character
+	return character;
 }
-
-// This event handler is called when the client is ready
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
 
 // This event handler is called when the client receives a message
 client.on("messageCreate", function(msg) {
-  // If the message is "!dndchar"
-  if (msg.content === '!dndchar') {
-    // Generate a random character and send it to the channel
-    const character = generateCharacter();
-    const characterEmbed = new EmbedBuilder()
-      .setTitle(`${character.name} the ${character.race} ${character.class}`)
-      .setDescription(`${character.name} is a ${character.alignment} character with the following abilities:
-
-        - Strength: ${character.abilities.strength}
-        - Dexterity: ${character.abilities.dexterity}
-        - Constitution: ${character.abilities.constitution}
-        - Intelligence: ${character.abilities.intelligence}
-        - Wisdom: ${character.abilities.wisdom}
-        - Charisma: ${character.abilities.charisma}
-
-        ${character.backstory}`)
-      .setColor(0x00AE86)
-      .setFooter({ text: 'Powered by chocy milk' })
-      msg.channel.send({ embeds: [characterEmbed] });
-
-  }
+	// If the message is "!dndchar"
+	if (msg.content === '!dndchar') {
+	// Generate a random character and send it to the channel
+	const character = generateCharacter();
+	
+	// Create the character embed with the generated character's information
+	const characterEmbed = new EmbedBuilder()
+	  .setTitle(`${character.name} the ${character.race} ${character.class}`)
+	  .setDescription(`${character.name} is a ${character.alignment} character with the following abilities:
+	
+		- Strength: ${character.abilities.strength}
+		- Dexterity: ${character.abilities.dexterity}
+		- Constitution: ${character.abilities.constitution}
+		- Intelligence: ${character.abilities.intelligence}
+		- Wisdom: ${character.abilities.wisdom}
+		- Charisma: ${character.abilities.charisma}
+	
+	  ${character.backstory}`)
+	  .setColor(0x00AE86)
+	  .setFooter({ text: 'Powered by chocy milk' });
+	
+	// Send the character embed to the channel
+	msg.channel.send({ embeds: [characterEmbed] });
+	
+	}
 });
 
 client.login(config.discord_token);
