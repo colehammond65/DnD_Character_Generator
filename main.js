3ae8908707cde7411d74b8e7a3f407510cf4acaf
Cole Hammond <cole.d.hammond@hotmail.co.nz>
2022-12-07 14:59:40 +1300

    0.0.4 - Cleaned up generator code

--- main.js
+++ main.js
@@ -14,61 +14,66 @@ client.on('ready', () => {
 });
 
 // Generate a random DnD character
-function generateCharacter() {
-	// Create an empty character object
-	const character = {};
-	
-	// Generate random abilities
-	character.abilities = {
-	strength: Math.floor(Math.random() * 20) + 1,
-	dexterity: Math.floor(Math.random() * 20) + 1,
-	constitution: Math.floor(Math.random() * 20) + 1,
-	intelligence: Math.floor(Math.random() * 20) + 1,
-	wisdom: Math.floor(Math.random() * 20) + 1,
-	charisma: Math.floor(Math.random() * 20) + 1
-	};
-	
-	// Read the names, races, classes, and alignments from their respective files
-	const names = fs.readFileSync('names.txt', 'utf8').split('\n');
-	const races = fs.readFileSync('races.txt', 'utf8').split('\n');
-	const classes = fs.readFileSync('classes.txt', 'utf8').split('\n');
-	const alignments = fs.readFileSync('alignments.txt', 'utf8').split('\n');
-	
-	// Generate a random name, race, class, and alignment
-	character.name = names[Math.floor(Math.random() * names.length)];
-	character.race = races[Math.floor(Math.random() * races.length)];
-	character.class = classes[Math.floor(Math.random() * classes.length)];
-	character.alignment = alignments[Math.floor(Math.random() * alignments.length)];
+async function generateCharacter() {
+  // Create an empty character object
+  const character = {};
 
-	// remove new line from name, race, class, and alignment
-	character.name = character.name.replace(/\r?\n|\r/g, " ");
-	character.race = character.race.replace(/\r?\n|\r/g, " ");
-	character.class = character.class.replace(/\r?\n|\r/g, " ");
-	character.alignment = character.alignment.replace(/\r?\n|\r/g, " ");
-	
-	// Generate character's backstory
-	character.backstory = "";
-	
-	// Read the past events, defining moments, and goals from their respective files
-	const pastEvents = fs.readFileSync('pastEvents.txt', 'utf8').split('\n');
-	const definingMoments = fs.readFileSync('definingMoments.txt', 'utf8').split('\n');
-	const goals = fs.readFileSync('goals.txt', 'utf8').split('\n');
-	
-	// Generate a random past event, defining moment, and goal
-	var past = pastEvents[Math.floor(Math.random() * pastEvents.length)];
-	var definingMoment = definingMoments[Math.floor(Math.random() * definingMoments.length)];
-	var goal = goals[Math.floor(Math.random() * goals.length)];
+  // Generate random abilities
+  character.abilities = {
+    strength: Math.floor(Math.random() * 20) + 1,
+    dexterity: Math.floor(Math.random() * 20) + 1,
+    constitution: Math.floor(Math.random() * 20) + 1,
+    intelligence: Math.floor(Math.random() * 20) + 1,
+    wisdom: Math.floor(Math.random() * 20) + 1,
+    charisma: Math.floor(Math.random() * 20) + 1
+  };
 
-	// remove new line from name, race, class, and alignment
-	past = past.replace(/\r?\n|\r/g, " ");
-	definingMoment = definingMoment.replace(/\r?\n|\r/g, " ");
-	goal= goal.replace(/\r?\n|\r/g, " ");
-	
-	// Add the past event, defining moment, and goal to the character's backstory
-	character.backstory += `${character.name} ${past}. One defining moment in their past was when they ${definingMoment}. Currently, ${character.name} is ${goal}.`;
-	
-	// Return the character
-	return character;
+  // Read the names, races, classes, and alignments from their respective files
+  const names = await fs.readFile('names.txt', 'utf8').split('\n');
+  const races = await fs.readFile('races.txt', 'utf8').split('\n');
+  const classes = await fs.readFile('classes.txt', 'utf8').split('\n');
+  const alignments = await fs.readFile('alignments.txt', 'utf8').split('\n');
+
+  // Generate a random name, race, class, and alignment
+  character.name = names[Math.floor(Math.random() * names.length)];
+  character.race = races[Math.floor(Math.random() * races.length)];
+  character.class = classes[Math.floor(Math.random() * classes.length)];
+  character.alignment = alignments[Math.floor(Math.random() * alignments.length)];
+
+  // remove new line from name, race, class, and alignment
+  character.name = removeNewLine(character.name);
+  character.race = removeNewLine(character.race);
+  character.class = removeNewLine(character.class);
+  character.alignment = removeNewLine(character.alignment);
+
+  // Generate character's backstory
+  character.backstory = "";
+
+  // Read the past events, defining moments, and goals from their respective files
+  const pastEvents = await fs.readFile('pastEvents.txt', 'utf8').split('\n');
+  const definingMoments = await fs.readFile('definingMoments.txt', 'utf8').split('\n');
+  const goals = await fs.readFile('goals.txt', 'utf8').split('\n');
+
+  // Generate a random past event, defining moment, and goal
+  var past = pastEvents[Math.floor(Math.random() * pastEvents.length)];
+  var definingMoment = definingMoments[Math.floor(Math.random() * definingMoments.length)];
+  var goal = goals[Math.floor(Math.random() * goals.length)];
+
+  // remove new line from name, race, class, and alignment
+  past = removeNewLine(past);
+  definingMoment = removeNewLine(definingMoment);
+  goal = removeNewLine(goal);
+
+  // Add the past event, defining moment, and goal to the character's backstory
+  character.backstory += ${character.name} ${past}. One defining moment in their past was when they ${definingMoment}. Currently, ${character.name} is ${goal}.;
+
+// Return the character
+  return character;
+}
+
+// Function to remove new line characters from a string
+  function removeNewLine(str) {
+  return str.replace(/\r?\n|\r/g, " ");
 }
 
 // This event handler is called when the client receives a message
